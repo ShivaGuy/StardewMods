5bcd7d057b1c4699994504cc8e814b2fa499f300
Kumar Chandresh <kumarchandreshsingh@gmail.com>
2022-04-27 18:40:50 +0530

    Add CustomTrashCan

---
+++ CustomTrashCan/ModConfig.cs
@@ -0,0 +1,11 @@
+namespace ShivaGuy.Stardew.CustomTrashCan
+{
+    public class ModConfig
+    {
+        public float Level0Multiplier = 0.00f;
+        public float Level1Multiplier = 0.15f;
+        public float Level2Multiplier = 0.30f;
+        public float Level3Multiplier = 0.45f;
+        public float Level4Multiplier = 0.60f;
+    }
+}
---
+++ CustomTrashCan/docs/ChangeLog.md
@@ -0,0 +1,6 @@
+# Release Notes
+
+## 1.0.0
+---
+Released on 27 April 2022 for SMAPI 3.0.0 or later.
+- Initial release.
